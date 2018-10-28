379cf72dfdab197d5d204607a775cbcaf49c62ae
MartinBG <MartinBG@abv.bg>
2018-10-28 03:14:27 +0300

    done task #4

--- 03. jQuery/Exercise/04. Form Validation/formValidation.js
+++ 03. jQuery/Exercise/04. Form Validation/formValidation.js
@@ -1,3 +1,58 @@
 function validate() {
-  // TODO
+  const username = $('#username');
+  const email = $('#email');
+  const password = $('#password');
+  const confirmPass = $('#confirm-password');
+  const submitBtn = $('#submit');
+  const company = $('#company');
+  const companyInfo = $('#companyInfo');
+  const companyNumber = $('#companyNumber');
+  let fieldsAreValid = true;
+
+  company.on('change', () => {
+    if (company.is(':checked')) {
+      companyInfo.css('display', 'block');
+    } else {
+      companyInfo.css('display', 'none');
+    }
+  });
+
+  submitBtn.on('click', (event) => {
+    event.preventDefault();
+    checkInputFields();
+    showMessage();
+    fieldsAreValid = true;
+  });
+
+  function checkInputFields() {
+    setBorder(username, /^[0-9A-Za-z]{3,20}$/.test(username.val()));
+    setBorder(email, /^.*@.*?[.]+.*$/.test(email.val()));
+    setBorder(password, /^(\w{5,15})$/.test(password.val()) && (password.val() === confirmPass.val()));
+    setBorder(confirmPass, /^(\w{5,15})$/.test(confirmPass.val()) && (password.val() === confirmPass.val()));
+    validateCompanyNumber();
+  }
+
+  function setBorder(element, isValid) {
+    if (isValid) {
+      element.css('border', 'none');
+    } else {
+      element.css('border', 'solid red');
+      fieldsAreValid = false;
+    }
+  }
+
+  function validateCompanyNumber() {
+    if (company.is(':checked')) {
+      const number = +companyNumber.val();
+      setBorder(companyNumber, number >= 1000 && number <= 9999);
+    }
+  }
+
+  function showMessage() {
+    if (fieldsAreValid) {
+      $('#valid').css('display', 'block');
+    } else {
+      $('#valid').css('display', 'none');
+    }
+  }
 }

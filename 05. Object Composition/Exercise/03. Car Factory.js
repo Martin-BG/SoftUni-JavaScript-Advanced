df93bf35b87379b52fa84960f3aad44900c66414
MartinBG <MartinBG@abv.bg>
2018-10-29 16:20:37 +0200

    done task #3

---
+++ 05. Object Composition/Exercise/03. Car Factory.js
@@ -0,0 +1,30 @@
+function carFactory(carProperties) {
+  let engine;
+  if (+carProperties.power <= 90) {
+    engine = {power: 90, volume: 1800};
+  } else if (+carProperties.power <= 120) {
+    engine = {power: 120, volume: 2400};
+  } else if (+carProperties.power <= 200) {
+    engine = {power: 200, volume: 3500};
+  }
+
+  const wheelSize = +carProperties.wheelsize % 2 === 0 ? +carProperties.wheelsize - 1 : +carProperties.wheelsize;
+
+  return {
+    model: carProperties.model,
+    engine,
+    carriage: {
+      type: carProperties.carriage,
+      color: carProperties.color,
+    },
+    wheels: [wheelSize, wheelSize, wheelSize, wheelSize]
+  };
+}
+
+console.log(carFactory({
+  model: 'VW Golf II',
+  power: 90,
+  color: 'blue',
+  carriage: 'hatchback',
+  wheelsize: 14
+}));

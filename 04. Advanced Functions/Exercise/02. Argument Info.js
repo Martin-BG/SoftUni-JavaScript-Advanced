d9cabda634d3afe3befc065d7b0de13da128b3ba
MartinBG <MartinBG@abv.bg>
2018-10-31 01:10:53 +0200

    done task #2

---
+++ 04. Advanced Functions/Exercise/02. Argument Info.js
@@ -0,0 +1,17 @@
+function argumentInfo() {
+  const typeCounts = {};
+  for (let arg of arguments) {
+    console.log(`${typeof(arg)}: ${arg}`);
+    if (!typeCounts[typeof(arg)]) {
+      typeCounts[typeof(arg)] = 1;
+    } else {
+      typeCounts[typeof(arg)]++;
+    }
+  }
+
+  Object.keys(typeCounts)
+    .sort((a, b) => typeCounts[b] - typeCounts[a])
+    .forEach(k => console.log(`${k} = ${typeCounts[k]}`));
+}
+
+argumentInfo('cat', 42, function () { console.log('Hello world!'); });

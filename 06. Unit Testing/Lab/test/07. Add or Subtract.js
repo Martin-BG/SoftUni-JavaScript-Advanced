e88f18bd0c3abde7446745c84e2900fedff73cf4
MartinBG <MartinBG@abv.bg>
2018-11-07 00:36:16 +0200

    refactoring

--- 06. Unit Testing/Lab/test/07. Add or Subtract.js
+++ 06. Unit Testing/Lab/test/07. Add or Subtract.js
@@ -8,20 +8,41 @@ describe('createCalculator()', () => {
     calc = createCalculator();
   });
 
-  it('should have get() function', () => {
-    const hasProp = calc.hasOwnProperty('get');
-    assert.isTrue(hasProp);
+  it('should return object with all properties as functions', () => {
+    const properties = ['get', 'add', 'subtract'];
+
+    assert.isObject(calc);
+
+    assert.containsAllKeys(calc, properties);
+
+    Object.keys(calc).forEach((key) => {
+      assert.isFunction(calc[key]);
+    });
   });
 
-  it('should have subtract() function', () => {
-    const hasProp = calc.hasOwnProperty('subtract');
-    assert.isTrue(hasProp);
+  it('should return object', () => {
+    assert.isObject(calc);
   });
 
-  it('should have add() function', () => {
-    const hasProp = calc.hasOwnProperty('add');
-    assert.isTrue(hasProp);
-  });
+  /*
+    it('should have get() function', () => {
+      const hasProp = calc.hasOwnProperty('get');
+      assert.isTrue(hasProp);
+      assert.isFunction(calc.get);
+    });
+
+    it('should have subtract() function', () => {
+      const hasProp = calc.hasOwnProperty('subtract');
+      assert.isTrue(hasProp);
+      assert.isFunction(calc.subtract);
+    });
+
+    it('should have add() function', () => {
+      const hasProp = calc.hasOwnProperty('add');
+      assert.isTrue(hasProp);
+      assert.isFunction(calc.add);
+    });
+  */
 
   it('should return 0 after create', () => {
     const value = calc.get();
@@ -42,11 +63,11 @@ describe('createCalculator()', () => {
     assert.equal(value, -5);
   });
 
-  it('should return 4.199999999999999 after {add 5.3; subtract 1.1}', () => {
+  it('should return 4.2 after {add 5.3; subtract 1.1}', () => {
     calc.add(5.3);
     calc.subtract(1.1);
     const value = calc.get();
-    assert.equal(value, 4.199999999999999);
+    assert.closeTo(value, 4.2, 0.00000001);
   });
 
   it('should return 2 after {add 10; subtract \'7\'\' add \'-2\'; subtract -1}', () => {

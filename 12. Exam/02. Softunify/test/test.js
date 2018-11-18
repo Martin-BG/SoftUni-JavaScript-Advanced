0f28c724d30ae96d29f52496e3456162590d2ea9
MartinBG <MartinBG@abv.bg>
2018-11-18 16:10:34 +0200

    skwleton

---
+++ 12. Exam/02. Softunify/test/test.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const Calculator = require('../Calculator').Calculator; // module.exports = {Calculator};
+
+describe('Calculator class', () => {
+  let calculator = new Calculator();
+
+  beforeEach('init HolidayPackage class', () => {
+    calculator = new Calculator();
+  });
+
+  it('', () => {
+
+  })
+});
